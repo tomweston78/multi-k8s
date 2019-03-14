14a6c28a6c8cb2c4e2ad60665a6f82bbc877ccd5
Tom Weston <tomweston78@yahoo.com>
2019-03-14 12:23:56 -0400

    initial k8s commit

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h3 className="App-title">Fib Calculator</h3>
+            <h3 className="App-title">Fib Calculator v.2</h3>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other page</Link>
           </header>
